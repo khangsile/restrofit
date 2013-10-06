cff86c68f9d7750f73103743be832df0573b0408
Khang Le <khangsile@gmail.com>
2013-10-06 00:57:54 -0400

    initial commit

---
+++ src/com/llc/restrofit/Restrofit.java
@@ -0,0 +1,44 @@
+package com.llc.restrofit;
+
+import java.util.Date;
+
+import retrofit.RestAdapter;
+import retrofit.converter.GsonConverter;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.internal.bind.DateTypeAdapter;
+
+/**
+ * A Retrofit Adapter that stores the baseURL and allows you to reuse the same restAdapter as opposed
+ * to reinstantiating it.
+ * @author KhangSiLe
+ *
+ */
+public class Restrofit {
+	
+	private static RestAdapter restAdapter = null;
+	
+	public static RestAdapter sharedAdapter() {
+		return restAdapter;
+	}
+	
+	public static RestAdapter defaultAdapter(String baseURL) {
+		Gson gson = new GsonBuilder()
+    		.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+    		.registerTypeAdapter(Date.class, new DateTypeAdapter())
+    		.create();
+
+		restAdapter = new RestAdapter.Builder()
+    		.setServer(baseURL)
+    		.setConverter(new GsonConverter(gson))
+    		.build();
+		
+		return restAdapter;
+	}
+	
+	public static void setSharedAdapter(RestAdapter adapter) {
+		restAdapter = adapter;
+	}
+}
